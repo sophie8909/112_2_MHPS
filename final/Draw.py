915a25f6ce528024d9d16b5c9458411d3b1d54b9
smallant0517 <59324695+smallant0517@users.noreply.github.com>
2024-05-06 15:41:46 +0800

    Update Draw.py

--- final/Draw.py
+++ final/Draw.py
@@ -7,9 +7,22 @@ class StringArtDrawer:
     def __init__(self, input_image):
         self.nails = [] #儲存各個釘子的x,y座標及灰階值，如[(100,200,150)]，第一個釘子的xy座標為(100,200)，其灰階值為150
         self.image = self.tocircle(input_image)
-        
-    def initialize_nails(self): #初始圖中的釘子
-        pass
+        self.num_nails = 288 #先寫在這，之後要放參數檔中
+    
+    def initialize_nails(self, image): #初始圖中的釘子
+        _, thresholded = cv2.threshold(image, 240, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        circle_contour = contours[0]  # 假設只有一個圓形
+        points = np.linspace(0, 2*np.pi, self.num_nails, endpoint=False)
+
+        for i in range(self.nums_nails):
+            angle = points[i]
+            x = int(circle_contour[0][0][0] + 0.9 * np.cos(angle))
+            y = int(circle_contour[0][0][0] + 0.9 * np.sin(angle))
+
+            gray_value = image[y, x]
+            self.nails.append((x, y, gray_value))
 
     def connect_nails(self, nail1, nail2): #nail1、nail2分別為兩釘子的x,y座標，將兩釘子連接成線
         pass
