58f3cf4247fee8eab70c0c16d083996edbb95f65
smallant0517 <59324695+smallant0517@users.noreply.github.com>
2024-05-06 14:37:52 +0800

    Update Draw.py

--- final/Draw.py
+++ final/Draw.py
@@ -7,5 +7,8 @@ class StringArtDrawer:
     def __init__(self):
         self.nails = [] #儲存各個釘子的x,y座標及灰階值，如[(100,200,150)]，第一個釘子的xy座標為(100,200)，其灰階值為150
 
-    def init_nails_positions(self):
+    def initialize_nails(self): #初始圖中的釘子
+        pass
+
+    def connect_nails(self, nail1, nail2): #nail1、nail2分別為兩釘子的x,y座標，將兩釘子連接成線
         pass
