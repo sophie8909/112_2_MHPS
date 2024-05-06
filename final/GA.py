6554f8ca6691c9dfbce8e8d3f386db8f6340cd5e
KaoZhai <60778840+KaoZhai@users.noreply.github.com>
2024-05-06 16:55:55 +0800

    add_GA_basic_structure

--- final/GA.py
+++ final/GA.py
@@ -1,1 +1,31 @@
-# Main GA program  
+import numpy as np
+import random
+import copy
+import json
+import time
+import cv2
+from Evaluate import Evaluate
+PARAMETERS = {}
+
+def nonDominatedSorting(populationSize, chromsObjRecord):
+    pass
+
+def CalculateCrowdingDistance(front, chromsObjRecord):
+    pass
+
+def selection(populationSize, front, chromsObjRecord, totalChromosome):
+    pass
+
+def initPopulation(populationSize):
+    pass
+
+def crossover(parent1, parent2, crossoverRate):
+    pass
+
+def mutation(original):
+    pass
+
+
+if __name__ == "__main__":
+    with open("GA_config.json", "r") as f:
+        PARAMETERS = json.load(f)
