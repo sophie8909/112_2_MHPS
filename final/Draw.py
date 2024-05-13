825cbb43ea9efbc6657e4c32bd4a83bfd4b29ac8
sophie8909 <34826797+sophie8909@users.noreply.github.com>
2024-05-13 16:00:40 +0800

    fix: draw.py

--- final/Draw.py
+++ final/Draw.py
@@ -1,13 +1,19 @@
 import cv2
 import numpy as np
-
+import json
 
 # draw the result of the string art
 class StringArtDrawer:
-    def __init__(self, input_image, config_file = "Draw_config.json"):
+    def __init__(self, input_image, config_file = "Draw_config.json", width=255, height=255):
         self.nails = [] #儲存各個釘子的x,y座標及灰階值，如[(100,200,150)]，第一個釘子的xy座標為(100,200)，其灰階值為150
-        self.image = self.tocircle(input_image)
-        self.num_nails = config_file["num_nails"]
+        self.width = width
+        self.height = height
+        self.image = self.resize(input_image)
+        self.image = self.tocircle(self.image)
+        # read config file
+        with open(config_file, "r") as f:
+            config_file = json.load(f)
+            self.num_nails = config_file["num_nails"]
         
     
     def initialize_nails(self): #初始圖中的釘子
@@ -43,13 +49,17 @@ class StringArtDrawer:
         cv2.line(self.image, start1, end1, (0, 0, 0), thickness=1)
         cv2.line(self.image, start2, end2, (0, 0, 0), thickness=1)
 
+    def resize(self, image):
+        return cv2.resize(image, (self.width, self.height), interpolation=cv2.INTER_AREA)
+
     def tocircle(self, input_image):
         height, width = input_image.shape
         radius = width // 2
-        output_image = np.zeros((height, width, 3), dtype=np.uint8) #創建一個空白的圓形圖片
+        output_image = np.zeros((height, width), dtype=np.uint8) #創建一個空白的圓形圖片
+
         cv2.circle(output_image, (width // 2, height // 2), radius, (255, 255, 255), -1)
         output_image = cv2.bitwise_and(input_image, output_image)
-        output_image = cv2.cvtColor(output_image, cv2.COLOR_BGR2GRAY) #轉灰階圖
+        # output_image = cv2.cvtColor(output_image, cv2.COLOR_BGR2GRAY) #轉灰階圖
         return output_image
 
     # decode the population to np.ndarray
